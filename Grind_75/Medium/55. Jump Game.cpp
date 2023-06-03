9163530e850693c15c6ee39f863f5790955d1057
SirHauler <realalexalvarado@gmail.com>
2023-06-03 11:59:59 -0700

    jump game

---
+++ Grind_75/Medium/55. Jump Game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reachable = 0; 
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+
+            if (i > reachable) {
+                return false;
+            }
+            
+            reachable = max(reachable, i + nums[i]); 
+
+            if (reachable >= n - 1) {
+                // then we can reach the end
+                return true;
+            }
+
+        }
+
+
+        return false;
+    }
+};
